3c9c706badba88f25855b901d6ed91332415275d
ijadams <ijadams503@gmail.com>
2020-09-20 20:41:33 -0500

    add files

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,33 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="container">
-                <div className="social">
-                    <ul>
-                        <li>
-                        <span
-                            uk-icon="icon: facebook; ratio: 2"></span>
-                        </li>
-                        <li>
-                        <span
-                            uk-icon="icon: twitter; ratio: 2"></span>
-                        </li>
-                        <li>
-                        <span
-                            uk-icon="icon: linkedin; ratio: 2"></span>
-                        </li>
-                        <li>
-                        <span
-                            uk-icon="icon: youtube; ratio: 2"></span>
-                        </li>
-                    </ul>
-                </div>
-                <div className="copyright">
-                    <p>© 2020 Envelope, Inc. All rights reserved.Terms, Privacy, CA Privacy, Copyright, & Cookies</p>
-                </div>
-            </div>
-        </div>
+        <div className="footer"></div>
     );
 };
 
