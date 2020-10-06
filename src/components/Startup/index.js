7fbea066a4e1428a8fc865b0cc4df40526d2c461
Ian Adams <ijadams503@gmail.com>
2020-10-06 17:42:31 -0500

    loader

--- src/components/Startup/index.js
+++ src/components/Startup/index.js
@@ -3,7 +3,7 @@ import Background from "../Background";
 import Query from "../Query";
 import LOADER_QUERY from "../../queries/loader/loader";
 const Startup = () => {
-    const url = process.env.REACT_APP_BACKEND_URL;
+    const url = process.env.NODE_ENV !== "development" ? '' : process.env.REACT_APP_BACKEND_URL;
     return (
         <Query query={LOADER_QUERY}>
             {({data: {loader}}) => {
