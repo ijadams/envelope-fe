a246db79aec18b13e91bcb3f2383ceeffb515fa7
Ian Adams <ijadams503@gmail.com>
2020-10-06 16:23:45 -0500

    add space to lettering

--- src/components/Lettering/index.js
+++ src/components/Lettering/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Lettering = ({ text = [], title = '' }) => {
     return (
         <div className="lettering">
-            <p><span className="title">{title}</span>{text}</p>
+            <p><span className="title">{title} </span>{text}</p>
         </div>
     );
 };
