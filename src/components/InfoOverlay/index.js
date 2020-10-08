47c6484cf02ca43a68fd765fed8b008d6904443e
Ian Adams <ijadams503@gmail.com>
2020-10-08 14:40:03 -0500

    md render

--- src/components/InfoOverlay/index.js
+++ src/components/InfoOverlay/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {navService} from "../../services";
+import Query from "../Query";
+import INFO_QUERY from "../../queries/info/info";
+import ReactMarkdown from "react-markdown";
 
 export const InfoOverlay = (props) => {
 
@@ -8,10 +11,23 @@ export const InfoOverlay = (props) => {
     }
 
     return (
-        <div className={`nav--overlay ${props.active ? "active" : ""}`}>
-            <div className="close--container" onClick={closeNav} >
-                <img src="https://ijadams.s3.amazonaws.com/envelope/info-black.png" alt="chevron"/>
-            </div>
-        </div>
+        <Query query={INFO_QUERY}>
+            {({data: {info}}) => {
+                return (
+                    <div className={`nav--overlay ${props.active ? "active" : ""}`}>
+                        <div className="close--container" onClick={closeNav}>
+                            <img src="https://ijadams.s3.amazonaws.com/envelope/info-black.png" alt="chevron"/>
+                        </div>
+                        <div className="top">
+                            <ReactMarkdown source={info.top}/>
+                        </div>
+                        <div className="bottom">
+                            <ReactMarkdown source={info.bottom}/>
+                        </div>
+                    </div>
+                )
+            }}
+        </Query>
     );
+
 };
