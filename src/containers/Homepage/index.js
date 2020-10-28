63b92a81916247b67266d043a1669366dfdfbf8d
Ian Adams <ijadams503@gmail.com>
2020-10-28 14:45:29 -0500

    swipe fixes

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -4,6 +4,7 @@ import PROJECTS_QUERY from "../../queries/projects/projects";
 import Project from '../../components/Project/index';
 import Startup from "../../components/Startup";
 import {navService} from "../../services";
+import { Swipeable } from 'react-swipeable'
 
 export class Homepage extends Component {
     constructor() {
@@ -12,6 +13,7 @@ export class Homepage extends Component {
             projectsLoaded: false,
             activeIndex: 0,
             sliderLoading: false,
+            mobileDragDisable: false,
             projectsLength: null,
             isMobile: false,
             touchStart: null,
@@ -43,6 +45,13 @@ export class Homepage extends Component {
             this.setState({
                 activeIndex: data.activeIndex
             });
+
+            setTimeout(() => {
+                this.setState({
+                    mobileDragDisable: false
+                });
+            }, 2250)
+
             setTimeout(() => {
                 this.setState({
                     sliderLoading: false
@@ -56,18 +65,26 @@ export class Homepage extends Component {
         if (Math.abs(event.deltaY) < 10 || this.state.sliderLoading) {
             return
         }
-        if (event.deltaY < 0 && this.state.activeIndex !== this.state.projectsLength - 1) {
-            this.setState({
-                sliderLoading: true
-            });
+        if (event.deltaY > 0 && this.state.activeIndex !== this.state.projectsLength - 1) {
             const i = this.state.activeIndex + 1;
             navService.setActiveIndex(i)
-        } else if (event.deltaY > 0 &&  this.state.activeIndex > 0) {
-            this.setState({
-                sliderLoading: true
-            });
+        } else if (event.deltaY < 0 && this.state.activeIndex > 0) {
             const i = this.state.activeIndex - 1;
             navService.setActiveIndex(i);
+        }
+    }
+
+    onSwipe(event) {
+        if (!this.state.sliderLoading && !this.state.mobileDragDisable) {
+            if (event.dir === "Up" && this.state.activeIndex !== this.state.projectsLength - 1) {
+                const i = this.state.activeIndex + 1;
+                navService.setActiveIndex(i)
+                this.setState({mobileDragDisable: true});
+            } else if (event.dir === "Down" && this.state.activeIndex > 0) {
+                const i = this.state.activeIndex - 1;
+                navService.setActiveIndex(i);
+                this.setState({mobileDragDisable: true});
+            }
         }
     }
 
@@ -78,27 +95,38 @@ export class Homepage extends Component {
 
     render() {
         const url = process.env.NODE_ENV !== "development" ? '' : process.env.REACT_APP_BACKEND_URL;
+        const config = {
+            delta: 10,                             // min distance(px) before a swipe starts
+            preventDefaultTouchmoveEvent: false,   // preventDefault on touchmove, *See Details*
+            trackTouch: true,                      // track touch input
+            trackMouse: false,                     // track mouse input
+            rotationAngle: 0,                      // set a rotation angle
+        };
+
         return (
             <div onWheel={(e) => this.onWheel(e)}>
-                <Startup loaded={this.state.projectsLoaded ? setTimeout(() => {
-                    return true;
-                }, 2500) : false}/>
-                <div className={`slider--overlay ${this.state.sliderLoading ? "active" : ""}`}></div>
-                <Query query={PROJECTS_QUERY}>
-                    {({data: {projects}}) => {
-                        return (
-                            <div>
-                                {this.state.projectsLoaded &&
-                                <section id="projects" className={`${this.state.sliderLoading ? "slide--loading" : ""}`}>
-                                    <Project data={projects[this.state.activeIndex]}
-                                             projectsLength={projects.length}
-                                             activeIndex={this.state.activeIndex} url={url} key={0}></Project>
-                                </section>
-                                }
-                            </div>
-                        );
-                    }}
-                </Query>
+                <Swipeable onSwiped={(eventData) => this.onSwipe(eventData)} {...config} >
+                    <Startup loaded={this.state.projectsLoaded ? setTimeout(() => {
+                        return true;
+                    }, 2500) : false}/>
+                    <div className={`slider--overlay ${this.state.sliderLoading ? "active" : ""}`}></div>
+                    <Query query={PROJECTS_QUERY}>
+                        {({data: {projects}}) => {
+                            return (
+                                <div>
+                                    {this.state.projectsLoaded &&
+                                    <section id="projects"
+                                             className={`${this.state.sliderLoading ? "slide--loading" : ""}`}>
+                                        <Project data={projects[this.state.activeIndex]}
+                                                 projectsLength={projects.length}
+                                                 activeIndex={this.state.activeIndex} url={url} key={0}></Project>
+                                    </section>
+                                    }
+                                </div>
+                            );
+                        }}
+                    </Query>
+                </Swipeable>
             </div>
         );
     }
